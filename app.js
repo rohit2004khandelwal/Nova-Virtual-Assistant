221d24b5775f029ecc7c1788648dcea272349dcd
Rohit khandelwal <rohit2004khandelwal@gmail.com>
2025-07-06 23:58:15 +0530

    added speak function

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let btn = document.querySelector('#btn');
 let content = document.querySelector('#content');
 function speak(text) {
-    let speech = new SpeechSynthesisUtterance();
-
+    let speech = new SpeechSynthesisUtterance(text);
+    speech.rate = 1;
+    speech.pitch = 1;
+    speech.volume = 1;
+    speech.lang = 'en-US';
+    window.speechSynthesis.speak(speech);
 }
